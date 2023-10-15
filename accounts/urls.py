b76fc422d849f365a8807011ac9c8c181b78e2a5
zaidkamil <xaidmetamorphos@gmail.com>
2023-10-15 19:32:23 +0530

    blog setup done

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    # htmx urls
+    path('login/htmx/', htmx_login, name='htmx_login'),
+    path('register/htmx/', htmx_register, name='htmx_register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,11 @@ def login(request):
 def register(request):
     ctx = {}
     return render(request, 'accounts/register.html', ctx)
+
+def htmx_login(request):
+    ctx = {}
+    return render(request, 'partials/login_form.html', ctx)
+
+def htmx_register(request):
+    ctx = {}
+    return render(request, 'partials/register_form.html', ctx)
